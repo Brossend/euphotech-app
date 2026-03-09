00f4a7a240e8b2388e620c9c75d0b4cf4d291222
i.lyamtsev <ilyamtsev@c-and-u.co>
2026-03-09 23:28:22 +0500

    feat(project): project initialization, installation, and configuration of dependencies

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-slate-900 text-red-500 p-12 rounded-lg shadow-lg text-left">
+    Tailwind работает 🚀
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  icon: {
+    customCollections: [
+      {
+        prefix: 'icons',
+        dir: './assets/icons',
+      },
+    ],
+  },
+  i18n: {
+    defaultLocale: 'ru',
+    locales: [
+      { code: 'ru', name: 'Russian', language: 'ru-RU', file: 'ru.json' },
+      { code: 'en', name: 'English', language: 'en-US', file: 'en.json' }
+    ],
+  },
+  modules: [
+    '@nuxt/a11y',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n'
+  ]
+})
